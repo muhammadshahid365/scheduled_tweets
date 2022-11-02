08f5b26be832da1dc5558b3c1a925e69799850fd
shahid <stylishcomputers.786@gmail.com>
2022-11-02 19:40:02 +0500

    make connect account to work again

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_26_101939) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.integer "twitter_account_id"
     t.integer "user_id"
--- vendor/bundle/ruby/3.0.0/gems/redis-client-0.10.0/lib/redis_client/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class RedisClient
-  VERSION = "0.10.0"
-end
